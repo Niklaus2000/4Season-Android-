aa5f3b0f96289edcdc8f27539257c8bd992dcc28
nika <78690921+Niklaus2000@users.noreply.github.com>
2021-11-12 01:11:40 +0400

    design1

---
+++ app/src/main/java/com/example/a4season/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a4season
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainActivity
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/a4season/OnBoardingItem.kt
@@ -0,0 +1,7 @@
+package com.example.a4season
+
+data class OnBoardingItem (
+    val onBoardingImage: Int,
+    val title: String,
+    val description: String
+)
